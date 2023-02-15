1903d61fa7063d36290d0748f44066749979ab0c
William Pedro <54008790+williamlab123@users.noreply.github.com>
2023-02-15 16:41:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+
+using System;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Threading;
 
 namespace EmailExample
 {
@@ -9,86 +11,110 @@ namespace EmailExample
     {
         static void Main(string[] args)
         {
-
             Console.ForegroundColor = ConsoleColor.Red;
-            System.Console.WriteLine("Email sender");
-            System.Console.WriteLine("Use carefully");
+            Console.WriteLine("Email sender");
+            Console.WriteLine("Use carefully");
             Console.ResetColor();
 
-            System.Console.WriteLine("Type your email - It must be an outlook.com");
+            Console.WriteLine("Type your email - It must be an outlook.com");
             string email = Console.ReadLine();
-            System.Console.WriteLine("Type your email password");
+            Console.WriteLine("Type your email password");
             string password = Console.ReadLine();
-          
-   
 
-           
-         
+            // Reset console color
             Console.ResetColor();
 
-            System.Console.WriteLine("Type the destination email");
+            Console.WriteLine("Type the destination email");
             string dest_email = Console.ReadLine();
 
-
-            System.Console.WriteLine("Type the email title or leave empty for a default title");
+            Console.WriteLine("Type the email title or leave empty for a default title");
             string eTitle = Console.ReadLine();
 
+            // If the email title is empty, set the title to "Email Title"
             if (eTitle.Length == 0)
             {
                 eTitle = "Email Title";
             }
 
-            System.Console.WriteLine("Type the email body or leave empty for a default body");
+            Console.WriteLine("Type the email body or leave empty for a default body");
             string eBody = Console.ReadLine();
 
+            // If the email body is empty, set the body to "Email Body"
             if (eBody.Length == 0)
             {
                 eBody = "Email Body";
             }
 
-
-
-            System.Console.WriteLine("Now type how many times you want to send the email");
+            // Asks the user to input the number of times they want to send the email
+            Console.WriteLine("Now type how many times you want to send the email");
             int times = int.Parse(Console.ReadLine());
 
+            // Loop that sends the email the specified number of times
             for (int i = 0; i < times; i++)
             {
-                try
+                //Makes the program sleeps for 700 milisecons
+                //NOTE: if you lower this value, your email may not be sent 
+                //because it will be target as spam
+                Thread.Sleep(700);
+
+                //Create a thread to send emails faster. 
+                //If you do not want to send faster, you can increase the value of 'Thread.Sleep()'
+                //or remove the threads.
+                Thread thread = new Thread(() =>
                 {
-                    MailMessage mail = new MailMessage();
-                    SmtpClient SmtpServer = new SmtpClient("smtp-mail.outlook.com");
-              
-
-                    mail.From = new MailAddress(email);
-                    mail.To.Add(dest_email);
-                    mail.Subject = eTitle;
-                    mail.Body = eBody;
-
-                    SmtpServer.Port = 587;
-                    SmtpServer.Credentials = new System.Net.NetworkCredential(email, password);
-                    SmtpServer.EnableSsl = true;
-
-                    SmtpServer.Send(mail);
-                    Console.WriteLine("email sent successfully");
-                }
-                catch (Exception ex)
-                {
-                    if (ex is SmtpFailedRecipientException)
+                    try
                     {
-                        Console.WriteLine("the email could not be found");
-                           System.Environment.Exit(0);
-                    }
-                    else
-                    {
-                        Console.WriteLine("an error occurred: " + ex.Message);
-                        System.Environment.Exit(0);
-                    }
-                }
+                        Console.WriteLine($"email {i} sent successfully!");
+                        // Create a new MailMessage object
+                        MailMessage mail = new MailMessage();
+                        // Create a new SmtpClient object using the Outlook.com SMTP server
+                        SmtpClient SmtpServer = new SmtpClient("smtp-mail.outlook.com");
 
+                        // Set the email address of the sender
+                        mail.From = new MailAddress(email);
+                        // Add the destination email address
+                        mail.To.Add(dest_email);
+
+                        //Use the line below to send a image
+
+                        //mail.Attachments.Add(new Attachment("your image path"));
+
+                        // Set the subject of the email
+                        mail.Subject = eTitle;
+                        // Set the body of the email
+                        mail.Body = eBody;
+
+                        // Set the port to use for the SMTP connection
+                        SmtpServer.Port = 587;
+                        // Set the email and password credentials to use for authentication
+                        SmtpServer.Credentials = new NetworkCredential(email, password);
+                        // Enable SSL encryption for the SMTP connection
+                        SmtpServer.EnableSsl = true;
+
+                        // Send the email
+                        SmtpServer.Send(mail);
+                        Console.WriteLine("email sent successfully");
+                    }
+                    //Catch the errors
+                    catch (Exception ex)
+                    {
+                        //If the email could not be found
+                        if (ex is SmtpFailedRecipientException)
+                        {
+                            Console.WriteLine("the email could not be found");
+                        }
+                        //if any other error occurs
+                        else
+                        {
+                            Console.WriteLine("an error occurred: " + ex.Message);
+                        }
+                    }
+                });
+                thread.Start();
             }
-               System.Environment.Exit(0);
 
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
-        
     }
 }
